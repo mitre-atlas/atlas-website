8907e8470506e1c10356b77ba775800b55d7e91a
Mouaz Alhindi <malhindi@mitre.org>
2021-04-08 16:49:42 -0400

    added capitalize first letter function

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -11,14 +11,19 @@ export default {
       items: []
     }
   },
+  methods: {
+    capitalizeFirstLetter (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
   mounted () {
     const pathItems = this.$route.path.split('/').slice(1)
     const homeItem = { href: '/', text: 'Home', exact: true }
-    const indexItem = { href: '/' + pathItems[0], text: pathItems[0], exact: true }
+    const indexItem = { href: '/' + pathItems[0], text: this.capitalizeFirstLetter(pathItems[0]), exact: true }
     this.items.push(homeItem)
     this.items.push(indexItem)
     if (pathItems.length > 1) {
-      const idItem = { href: '/' + pathItems[0] + '/' + pathItems[1], text: pathItems[1], exact: true }
+      const idItem = { href: '/' + pathItems[0] + '/' + pathItems[1], text: this.capitalizeFirstLetter(pathItems[1]), exact: true }
       this.items.push(idItem)
     }
   }
