54046a937a0f180cfc3e1b59b329385d8d8e6f70
Lily Wong <lwong@mitre.org>
2021-04-28 18:29:46 +0000

    typo

--- components/InfoTable.vue
+++ components/InfoTable.vue
@@ -15,7 +15,7 @@
         <v-switch
           v-if="this.$route.name === 'tactics' || this.$route.name === 'techniques'"
           v-model="showAdvMlOnly"
-          label="Show only ML"
+          label="Show only AdvML"
           class="pa-3"
         />
       </v-col>
