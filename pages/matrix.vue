135a1d37523cd12fb062a37ccdc23b9d243cd906
Lily Wong <lwong@mitre.org>
2021-06-09 19:41:29 +0000

    Matrix page review: lowercase tactics, techniques, short name, ATTACK trademark

--- pages/matrix.vue
+++ pages/matrix.vue
@@ -4,9 +4,9 @@
     <page-title>{{ title }}</page-title>
 
     <p class="mb-10">
-    {{ $config.name.long }}, or {{ $config.name.short }}, emphasizes adversarial attacks and attacks against ML systems. {{ $config.name.short }} recognizes that ML is an embedded technology and
-    retains the high-level Technique and Tactic categories found in the <a href="https://attack.mitre.org/matrices/enterprise/">ATT&CK Enterprise</a> matrix. The definitions
-    have been amended to emphasize the ML security aspects of each Technique. This enables {{ $config.name.short }} to adequately describe attacks against ML systems.
+    MITRE {{ $config.name.short }} emphasizes adversarial attacks and attacks against ML systems. {{ $config.name.short }} recognizes that ML is an embedded technology and
+    retains the high-level technique and tactic categories found in the <a href="https://attack.mitre.org/matrices/enterprise/">MITRE ATT&CK<sup>&reg;</sup> Enterprise Matrix</a>. The definitions
+    have been amended to emphasize the ML security aspects of each technique. This enables {{ $config.name.short }} to adequately describe attacks against ML systems.
     Additionally, relevant TTPs from the ATT&CK Enterprise have been included in this knowledge base.
     </p>
 
@@ -32,5 +32,8 @@ export default {
 <style scoped>
 .row {
   overflow: auto;
+}
+a {
+  text-decoration: none;
 }
 </style>
