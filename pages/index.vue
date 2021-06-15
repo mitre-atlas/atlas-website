9b70c34b5097b685ef531ac21b19dd93824eb4a4
Moruph Osuolale <mosuolale@mitre.org>
2021-06-15 02:10:04 +0000

    Add base options for link area

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,50 @@
     <page-title>{{ title }}</page-title>
 
     <v-row>
-      <v-col cols="8">
-        <p class="mb-10">
-        The {{ $config.name.short }} Threat Matrix is a knowledge base of adversarial tactics and techniques for machine learning (ML) security based on <a href="https://attack.mitre.org" style="text-decoration: none;">the ATT&CK framework</a>.
-        Along with case studies demonstrating attacks on ML systems, this knowledge base enables machine learning and security communities to better understand ML threats.
-        </p>
-      </v-col>
+      <p class="mb-10">
+      The {{ $config.name.short }} Threat Matrix is a knowledge base of adversarial tactics and techniques for machine learning (ML) security based on <a href="https://attack.mitre.org" style="text-decoration: none;">the ATT&CK framework</a>.
+      Along with case studies demonstrating attacks on ML systems, this knowledge base enables machine learning and security communities to better understand ML threats.
+      </p>
 
-      <v-col>
-        <div class="text-center mb-10">
-        <NuxtLink to="/page/adversarial-ml-101" style="text-decoration: none;">Adversarial ML 101</NuxtLink>
-          <br>
-          <NuxtLink to="/resources/contribute" style="text-decoration: none;">Contribute</NuxtLink>
-          <br>
-          <NuxtLink to="/resources/feedback" style="text-decoration: none;">Contact Us</NuxtLink>
-          <br>
-          <span><a :href="`${$config.navigator_url}`" target="_blank" style="text-decoration: none;">View on ATT&CK Navigator</a><v-icon small>mdi-open-in-new</v-icon></span>
-        </div>
-      </v-col>
+    </v-row>
+
+  <!--Two-row list, horizontal bar, outlined -->
+    <!-- <v-row justify="center">
+          <v-card outlined class="mb-10 mt-2 pa-3">
+            <v-chip color="transparent" nuxt=true class="ma-1"><NuxtLink to="/page/adversarial-ml-101" style="text-decoration: none;">Adversarial ML 101</NuxtLink></v-chip>
+            <v-chip color="transparent" nuxt=true class="ma-1"><NuxtLink to="/resources/contribute" style="text-decoration: none;">Contribute</NuxtLink></v-chip>
+            <hr>
+            <v-chip color="transparent" nuxt=true class="ma-1"><NuxtLink to="/resources/feedback" style="text-decoration: none;">Contact Us</NuxtLink></v-chip>
+            <v-chip color="transparent" class="ma-1" :href="`${$config.navigator_url}`"><span>View on ATT&CK Navigator</span></v-chip>
+          </v-card>
+    </v-row> -->
+
+  <!--Vertical list, outlined-->
+    <!-- <v-row justify="center">
+        <v-card outlined rounded="xl" class="mb-10 link-box">
+          <v-list class="link-box">
+            <v-list-item-group class="pa-3">
+                <v-list-item nuxt to="/page/adversarial-ml-101"><span class="link-box">Adversarial ML 101</span></v-list-item>
+                <v-list-item nuxt to="/resources/contribute"><span class="link-box">Contribute</span></v-list-item>
+                <v-list-item nuxt to="/resources/feedback"><span class="link-box">Contact Us</span></v-list-item>
+                <v-list-item class="ma-1" :href="`${$config.navigator_url}`"><span class="link-box">View on<span class="attack-style"> ATT&CK </span>Navigator</span></v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+    </v-row> -->
+
+  <!--Horizonal list, chip items-->
+    <v-row justify="center">
+        <v-card rounded="lg" outlined class="mb-10 mt-3 link-box">
+          <v-list class="link-box">
+            <v-list-item-group class="pa-3">
+                <v-chip outlined nuxt=true class="ma-1" to="/page/adversarial-ml-101">Adversarial ML 101</v-chip>
+                <v-chip outlined nuxt=true class="ma-1" to="/resources/contribute">Contribute</v-chip>
+                <v-chip outlined nuxt=true class="ma-1" to="/resources/feedback">Contact Us</v-chip>
+                <v-chip outlined class="ma-1" :href="`${$config.navigator_url}`"><span>View on ATT&CK Navigator</span></v-chip>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
     </v-row>
 
     <v-row>
@@ -37,8 +63,23 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+
 .row {
   overflow: auto;
 }
+
+.attack-style {
+  color: #c63f1f;
+  white-space: pre;
+}
+
+span.link-box {
+  background-color: transparent;
+}
+
+.link-box {
+  background-color: black;
+}
+
 </style>
