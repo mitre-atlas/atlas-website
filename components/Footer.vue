ac4f9f207b82f316e7a18ab6d00b19a2501134e9
Lily Wong <lwong@mitre.org>
2021-06-16 02:26:28 +0000

    Update footer links to text buttons, color same as Header

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
-  <v-footer>
-    <v-card flat tile width="100%">
+  <v-footer padless dark>
+    <v-card flat tile width="100%" color="grey darken-3"><!-- MTIRE Navy #0D2F4F -->
       <v-card-text class="text-center">
         MITRE ATLAS<sup>&trade;</sup> and MITRE ATT&CK<sup>&reg;</sup> are registered trademarks of The MITRE Corporation.
       </v-card-text>
       <v-card-actions class="justify-center">
         <span>
-        <NuxtLink to="/resources/privacy-policy">Privacy Policy</NuxtLink>
-        &nbsp; &nbsp;
-        <NuxtLink to="/resources/terms">Terms of Use</NuxtLink>
+          <v-btn text nuxt to="/resources/privacy-policy">Privacy Policy</v-btn>
+          &nbsp; &nbsp;
+          <v-btn text nuxt to="/resources/terms">Terms of Use</v-btn>
         </span>
       </v-card-actions>
     </v-card>
