c04262d6f0e1866007a73485d0da2ec6b481e1a1
Lily Wong <lwong@mitre.org>
2021-06-18 18:54:22 +0000

    Update terms of use to use ATT&CK license

--- pages/resources/terms.vue
+++ pages/resources/terms.vue
@@ -3,17 +3,12 @@
   <page-title>Terms of Use</page-title>
   <page-section-title>License</page-section-title>
   <p>
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-    <br>
-        <a href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a>
-    <br>
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+    The MITRE Corporation (MITRE) hereby grants you a non-exclusive, royalty-free license to use ATT&CK<sup>&reg;</sup>
+    for research, development, and commercial purposes. Any copy you make for such purposes is authorized
+    provided that you reproduce MITRE's copyright designation and this license in any such copy.
+  </p>
+  <p>
+      "&copy; 2021 The MITRE Corporation. This work is reproduced and distributed with the permission of The MITRE Corporation."
   </p>
 </div>
 </template>
