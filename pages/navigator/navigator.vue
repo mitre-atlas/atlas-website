d33cc922bc482b9908a38514a742725245dfc4a9
Sean Bartholomew <sbartholomew@mitre.org>
2021-06-18 18:09:02 +0000

    minor page updates to iframe nav page

--- pages/navigator/navigator.vue
+++ pages/navigator/navigator.vue
@@ -5,24 +5,26 @@
 
     <!-- Below is just placeholder text for future language, for now I just put some language to start with so that it was there -->
 
-    <!-- <p>
-      The Navigator application linked below allows for an interactive experience with the Matrix displayed on the <a href="/matrix">Matrix Page</a>.
-      The link below will redirect to a separate web page with the interactive Navigator.
-    </p> -->
-
     <p>
-        To view MITRE ATLAS data alongside MITRE ATT&CK<sup>&reg;</sup> Enterprise tactics and techniques, visit the ATLAS customized version of the ATT&CK Navigator here: <br><br><v-btn target="_blank" :href=$config.navigator_url>Interactive Navigator</v-btn>
+      To view MITRE ATLAS data alongside MITRE ATT&CK<sup>&reg;</sup> Enterprise tactics and techniques, visit the ATLAS customized version of the ATT&CK Navigator. It can be viewed on another page via the link below, or in the window further down this page.
     </p>
 
     <p>
-        This customized Navigator encodes the ATLAS techniques as a separate platform called "ATLAS", and allows users to create complex visualizations containing both machine learning and enterprise techniques. For more information on the standard ATT&CK Navigator and how to use it, you can visit the <a target="_blank" href ="https://github.com/mitre-attack/attack-navigator">project’s GitHub page here</a> as well as watch a short <a target="_blank" href="https://www.youtube.com/watch?v=pcclNdwG8Vs.">demo video on YouTube</a>.
+      <v-card-actions class="justify-center">
+        <v-btn target="_blank" :href=$config.navigator_url>
+          Interactive Navigator
+        </v-btn>
+      </v-card-actions>
     </p>
 
-    <!-- This customized Navigator encodes the ATLAS techniques as a separate “ML” platform, and allows users to create complex visualizations containing both machine learning and enterprise techniques. For more information on the standard ATT&CK Navigator and how to use it, you can visit the project’s GitHub page here  -- https://github.com/mitre-attack/attack-navigator -- as well as watch a short demo video on YouTube: https://www.youtube.com/watch?v=pcclNdwG8Vs. -->
+    <p>
+      This customized Navigator encodes the ATLAS techniques as a separate platform called "ATLAS", and allows users to create complex visualizations containing both machine learning and enterprise techniques. For more information on the standard ATT&CK Navigator and how to use it, you can visit the <a target="_blank" href="https://github.com/mitre-attack/attack-navigator">project’s GitHub page here</a> as well as watch a short <a target="_blank" href="https://www.youtube.com/watch?v=pcclNdwG8Vs.">demo video on YouTube</a>.
+    </p>
 
     <!-- <v-row  class="mt-10">
       <matrix-attack-style :matrix="getMatrix" />
     </v-row> -->
+
     <div style="text-align: center;">
       <iframe
         :src="`${$config.navigator_url}`"
