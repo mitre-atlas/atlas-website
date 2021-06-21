4fdab57ec4a2308d26e4c06fb981e7bb449290e5
Lily Wong <lwong@mitre.org>
2021-06-21 18:31:06 +0000

    Add router

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
-export default function ({ $config }) {
+export default function ({ app, $config }) {
   // Set up only if analytics ID is provided, i.e. in production
   if ($config.analytics_id) {
     Vue.use(VueGtag, {
       config: {
         id: $config.analytics_id
       }
-    })
+    },
+    app.router
+    )
   }
 }
