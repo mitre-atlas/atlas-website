591b6a60601a6c2e22179cbde26afa5931301536
Kate Hanson <khanson@mitre.org>
2021-06-24 15:35:48 +0000

    Broken down case study builder into components

---
+++ components/EditProcedure.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-timeline v-if="procedure.length" align-top dense>
+    <v-timeline-item v-for="(p, i) in procedure" :key="i" small>
+      <procedure-card :info="p" />
+    </v-timeline-item>
+    </v-timeline>
+</template>
+
+<script>
+export default {
+  name: 'EditProcedure',
+  props: ['procedure']
+}
+</script>
