40ce37e8fab8ef9500815d62833c18f0ebcf65de
Lily Wong <lwong@mitre.org>
2021-11-16 02:04:32 +0000

    Move CSB instructions from Contribute page to a dialog on CSB page

--- pages/resources/contribute.vue
+++ pages/resources/contribute.vue
@@ -12,26 +12,25 @@
 
     <page-section-title>Contribute Case Studies</page-section-title>
 
-    <p>We are especially excited for new case-studies! We look forward to contributions from both industry and academic researchers. Before submitting a case-study, consider that the attack:</p>
+    <p>We are especially excited for new case-studies! We look forward to contributions from both industry and academic researchers.</p>
 
-    <ol>
-      <li>Exploits one or more vulnerabilities that compromises the confidentiality, integrity or availability of ML system.</li>
-      <li>The attack was against a production, commercial ML system. This can be on MLaaS like Amazon, Microsoft Azure, Google Cloud AI, IBM Watson, or ML systems embedded in client/edge.</li>
-      <li>You have permission to share the information/published this research. Please follow the proper channels before reporting a new attack and make sure you are practicing responsible disclosure.</li>
-    </ol>
+    <p>
+      Use the <NuxtLink to="/studies/create">Case Study Builder</NuxtLink> to craft a submission file with details and technique mappings,
+      then email the file to <a href="mailto:atlas@mitre.org">atlas@mitre.org</a>.
+      The {{ $config.name.short }} team will review the submission and follow up with feedback.
+    </p>
 
-    <br>
+    <p>
+      <v-card-actions>
+         <v-btn nuxt to="/studies/create">Go to the case study builder</v-btn>
+      </v-card-actions>
+    </p>
 
-    If the case study passes all three requirements, the <NuxtLink to="/studies/create">Build Case Study</NuxtLink> page can be used to construct the case study for submission.
-    In cases where a .yaml file already exists, the case study form accepts .yaml uploads for editing. Otherwise, the form should be populated with as much detail as possible, including meta-data and a list of the procedures used in the case study. Optionally, references can be added to support the case study.
-    Finally the case study should be shared with the {{ $config.name.short }} team by downloading the created .yaml file and emailing it to <a href="mailto:atlas@mitre.org">atlas@mitre.org</a>.
-    See an example of the format below.
-
-    <br><br>
-
+    <!--
     <div class="center">
       <video controls src="/atlas_case_study_demo.mp4" />
     </div>
+    -->
 
     <page-section-title>Case study example</page-section-title>
     <case-study-example />
