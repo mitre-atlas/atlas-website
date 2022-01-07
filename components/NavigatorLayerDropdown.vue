af7595d060cf927e7426881292cdf11963bd8208
Lily Wong <lwong@mitre.org>
2022-01-07 13:19:41 -0500

    Match link style for navigator layer

--- components/NavigatorLayerDropdown.vue
+++ components/NavigatorLayerDropdown.vue
@@ -1,40 +1,50 @@
 <template>
-<v-menu offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                text
-                color="inherit"
-                style="padding: 0;"
-                v-bind="attrs"
-                v-on="on"
-              >
-                Navigator Layer
-                <v-icon right>mdi-menu-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item>
-                  <a
-                  @click="downloadNavigatorLayerFile"
-                  style="color: inherit"
-                  >Download</a><v-icon
-                      small
-                    >mdi-arrow-collapse-down</v-icon>
-              </v-list-item>
-              <v-list-item>
-                <a
-                  :href="
-                    $config.individual_case_study.navigator_link +
-                    study.id +
-                    $config.individual_case_study.suffix
-                  "
-                  target="_blank"
-                  style="color: inherit"
-                  >View<v-icon small>mdi-open-in-new</v-icon></a
-                >
-              </v-list-item>
-            </v-list>
-          </v-menu>
+  <v-menu offset-y>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        text
+        color="inherit"
+        style="padding: 0;"
+        v-bind="attrs"
+        v-on="on"
+      >
+        Navigator Layer
+        <v-icon right>mdi-menu-down</v-icon>
+      </v-btn>
+    </template>
+    <v-list>
+
+      <v-list-item @click="downloadNavigatorLayerFile">
+        <v-list-item-icon style="margin-right: 0px;">
+          <v-icon small>
+            mdi-arrow-collapse-down
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            Download as raw data (.json)
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item
+        :href="navigatorUrlIndividualCaseStudyLayer"
+        target="_blank"
+        >
+        <v-list-item-icon style="margin-right: 0px;">
+          <v-icon small>
+            mdi-open-in-new
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            View on ATLAS Navigator
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+    </v-list>
+  </v-menu>
 </template>
 
 <script>
@@ -43,15 +53,19 @@ import { downloadUrlAsFile } from 'static/data/tools.js'
 export default {
   name: 'NavigatorLayerDropdown',
   props: ['study'],
+  /* eslint-disable camelcase */
+  data ({ $config: { individual_case_study } }) {
+    return {
+      // Construct full URL to individual case study Navigator Layer file on GitHub
+      rawJsonIndividualCaseStudyLayer: individual_case_study.raw_link + this.study.id + individual_case_study.suffix,
+      navigatorUrlIndividualCaseStudyLayer: individual_case_study.navigator_link + this.study.id + individual_case_study.suffix
+    }
+  },
+  /* eslint-enable camelcase */
   methods: {
     downloadNavigatorLayerFile () {
-      // Construct full URL to individual case study Navigator Layer file on GitHub
-      const rawLink = this.$config.individual_case_study.raw_link
-      const id = this.study.id
-      const suffix = this.$config.individual_case_study.suffix
-      const url = rawLink + id + suffix
       // Serve up download of that file, named the same
-      downloadUrlAsFile(url)
+      downloadUrlAsFile(this.rawJsonIndividualCaseStudyLayer)
     }
   }
 }
