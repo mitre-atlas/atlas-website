052b5d81c4cda5dfee2b39bf71e81101e7168695
Lily Wong <lwong@mitre.org>
2022-03-16 15:11:17 +0000

    Lint ContactButton and access props directly

--- components/ContactButton.vue
+++ components/ContactButton.vue
@@ -1,31 +1,23 @@
 <template>
   <div>
-        <v-btn
-            :color= "colorProp"
-            dark
-            large
-            :href = "linkProp"
-            target = "_blank"
-        >
-        <v-icon left>
-            {{icon}}
-        </v-icon>
-            {{buttonText}}
-        </v-btn>
-    </div>
+    <v-btn
+      :color="color"
+      dark
+      large
+      :href="link"
+      target="_blank"
+    >
+      <v-icon left>
+        {{ icon }}
+      </v-icon>
+      {{ buttonText }}
+    </v-btn>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ContactButton',
-  props: ['color', 'icon', 'buttonText', 'link'],
-  computed: {
-    colorProp () {
-      return this.color
-    },
-    linkProp () {
-      return this.link
-    }
-  }
+  props: ['color', 'icon', 'buttonText', 'link']
 }
 </script>
