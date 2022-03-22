85328d3c4f22fbc15bf2a8e6223ef12e2ba8a9e7
atong <atong@mitre.org>
2022-03-22 19:59:54 +0000

    added handle to the icon and tooltip included

--- components/case-study-builder/EditProcedure.vue
+++ components/case-study-builder/EditProcedure.vue
@@ -1,8 +1,14 @@
 <template>
   <v-timeline v-if="procedureData.length" align-top dense>
-    <draggable :list="procedureData" :disabled="editing">
+    <draggable :list="procedureData" :disabled="editing" handle=".handle">
       <!-- Key with index and technique for uniqueness -->
       <v-timeline-item v-for="(p, i) in procedureData" :key="i + p.technique" small>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon class="fa fa-align-justify handle" v-bind="attrs" v-on="on">mdi-arrow-up-down</v-icon>
+          </template>
+          <span>Click here to drag procedures to sort them</span>
+        </v-tooltip>
         <edit-procedure-card
           :info="p"
           :editing="editing"
@@ -47,3 +53,21 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .handle {
+    float:right;
+    padding-top:2%;
+    padding-right: 1%;
+    z-index: 1;
+    cursor: -webkit-grab; /* Chrome 1-21, Safari 4+ */
+    cursor: -moz-grab; /* Firefox 1.5-26 */
+    cursor: grab;
+  }
+
+  .handle:active {
+    cursor: -webkit-grabbing;
+    cursor: -moz-grabbing;
+    cursor: grabbing;
+  }
+
+</style>
