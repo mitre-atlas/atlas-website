6f8db083c915bebcbd3c242e84ee20b0dc3ed3c5
Vivien Jamba <vjamba@mitre.org>
2023-11-16 19:40:01 +0000

    adding brand-compliant home page template

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-card
+      class="pa-12"
+      color="navy"
+      tile
+      width="100%"
+    >
+      <div class="ml-12 mr-12 mt-8 mb-6">
+        <h1
+          style="font-weight: bold; ; text-align: left"
+          class="white--text text-h1"
+        >
+          MITRE ATLAS
+        </h1>
+        <h2
+          class="lightBlue--text text-h4"
+          style="font-weight: bold; text-align: left"
+        >
+          NAVIGATING THE LANDSCAPE OF THREATS <br>
+          TO MACHINE LEARNING SYSTEMS
+        </h2>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { useDisplay } from "vuetify";
+import { computed } from 'vue' 
+
+const display = useDisplay();
+
+const mobile = computed(() => {
+  return display.mobile.value;
+});
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
-  <hello-world />
+  <div>
+    <HomeHero/>
+    
+    <div
+      class="pt-10 pr-12 pl-12 pb-10"
+    >
+      <v-row
+        color="white"
+        tile
+        class="pr-12 pl-12"
+        width="100%"
+        style="margin-bottom: 5%; text-align: left"
+      >
+        <v-col :cols="`${mobile ? 12 : 9}`">
+          <h3
+            style="font-weight: bold;"
+            class="black--text pt-8 p-10 mt-8"
+          >
+            MITRE ATLAS™ (Adversarial Threat Landscape for Artificial-Intelligence Systems)  is a globally accessible, living knowledge base of adversary tactics and techniques against Al-enabled svstems based on real-world attack observations and realistic demonstrations from Al red teams and security groups. ATLAS is modeled after and complementary to MITRE ATT&CK®, raising awareness of the rapidly evolving vulnerabilities of Al-enabled systems as they extend beyond cyber.
+          </h3>
+        </v-col>
+        <v-col class="pt-10 pr-12 pl-12 pb-10" :cols="`${mobile ? 12 : 3}`">
+          <img
+            justify="center"
+            src="../assets/compass.png"
+            height="180"
+            contain
+          />
+        </v-col>
+      </v-row>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import HomeHero from '../components/HomeHero.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    HomeHero,
   },
 });
 </script>
