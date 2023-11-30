f2bc19ac116929ed9d03b4c5e1959fb1623ddec0
Vivien Jamba <vjamba@mitre.org>
2023-11-30 14:49:32 -0500

    readded vue config for pages

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
+})
