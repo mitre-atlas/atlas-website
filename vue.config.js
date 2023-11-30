ce66c1c7f4df73c37958c6bb85a36d6fa31b1232
Vivien Jamba <vjamba@mitre.org>
2023-11-29 23:09:17 -0500

    add path to vue.config for gitlab deployment

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,8 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
 })
