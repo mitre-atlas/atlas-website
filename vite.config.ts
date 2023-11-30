d43b45d4d255c0fcbddc44d891308915a0f60f19
Vivien Jamba <vjamba@mitre.org>
2023-11-30 18:28:36 -0500

    reformatting base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'advml.pages.mitre.org/atlas-website-vue3',
+  base: 'https://advml.pages.mitre.org/atlas-website-vue3/',
   optimizeDeps: {
     esbuildOptions: {
       target: "esnext",
