d230add0a9703d85a7bed760e7b66a34d4722f0e
Vivien Jamba <vjamba@mitre.org>
2023-11-30 19:52:35 -0500

    Testing manual base url version commit b64c4b8c0c26a74e6e84e8960ea68749abdd588e

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: fileURLToPath(new URL('/', import.meta.url)),
+  base: '/atlas-website-vue3/',
   optimizeDeps: {
     esbuildOptions: {
       target: "esnext",
