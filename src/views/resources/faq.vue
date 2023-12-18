c36af7f242d89c884f6eb53d8f6bedcd7d8e36df
Vivien Jamba <vjamba@mitre.org>
2023-12-18 15:24:22 -0400

    cleaned up code based on MR comments

--- src/views/resources/faq.vue
+++ src/views/resources/faq.vue
@@ -3,30 +3,30 @@
   <div class="pa-7 text-left"> 
     <div class="text-h3">{{ title }}</div>
 
-    <div class="pl-4" v-for="(section, index) in Object.entries(faqSections)" :key="index">
-      <div class="pt-7 pb-2 text-h6"> {{ sectionTitles[index] }} </div>
-      <component class="pb-8 text-body-1" :is="section[1].default"></component>
+    <div class="pl-4" v-for="section in faqSections" :key="section.frontmatter.title">
+      <div class="pt-7 pb-2 text-h6"> {{ section.frontmatter.title }} </div>
+      <component class="pb-8 text-body-1" :is="section.default"></component>
     </div>
 
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 
 const faqSections = ref([]);
-const sectionTitles = ref([]);
 const title = 'FAQ'
 
 async function importFiles() {
+  // Get all names of files in the FAQ md folder
   const fileNames = Object.keys(
     import.meta.glob('@/../public/content/faq-files/*.md')
   );
 
+  // Collect markdown content from each file
   for (const fileName of fileNames) {
     try {
       const markdownContent = await import( /* @vite-ignore */ fileName);
-      sectionTitles.value.push(markdownContent.frontmatter.title)
       faqSections.value.push(markdownContent);
     } catch (error) {
       console.error(`Error importing ${fileName}:`, error);
@@ -34,10 +34,8 @@ async function importFiles() {
   }
 }
 
-// Call importFiles on component mount
-onMounted(async () => {
-  importFiles();
-});
+importFiles();
+
 </script>
 
 <style>
