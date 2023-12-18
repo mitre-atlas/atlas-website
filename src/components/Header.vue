8bce0335231f0b90f85a84e2867748fa71d2e9e5
henrychoy <hchoy@mitre.org>
2023-12-18 10:58:11 -0500

    added navigator page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,13 +12,13 @@
   <!-- <v-app-bar-nav-icon color="white" v-if="doesPageHaveSideNav" @click.prevent="toggle()" />  -->
 
   <h1 class="pa-3">
-    <a href="/atlas-website-vue3/techniques/AML.T0041">
+    <router-link to="/">
       <img
         src="../assets/MITRE-brand_ATLAS_MITRE_tm_white.svg"
         width="200"
         contain
       />
-    </a>
+    </router-link>
   </h1>
 
   <v-spacer />
