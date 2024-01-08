20c7b0a4745e0db4e7852f49ac617b14bcb957cc
Lily Wong <lwong@mitre.org>
2024-01-08 16:26:52 -0500

    Add markdownit rendering rules to apply Vuetify styles; increase FAQ headings to h6

--- src/plugins/markdownit.ts
+++ src/plugins/markdownit.ts
@@ -9,10 +9,48 @@ const markdownit = new MarkdownIt({
   })
   .use(frontmatterPlugin)
 
-// TODO Adding new rendering rules for Vuetify styling
+// Adding new rendering rules to apply Vuetify styling
 // https://github.com/markdown-it/markdown-it/blob/master/docs/examples/renderer_rules.md#adding-new-rules
-// markdownit.renderer.rules.table_open = () => '<v-table>'
-// markdownit.renderer.rules.table_close = () => '</v-table>'
+
+// Create proxy for rules modification
+const proxy = (tokens: any[], idx: number, options: object, env: object, self: any) => self.renderToken(tokens, idx, options);
+
+// Apply Vuetify text styling to paragraph elements
+// https://github.com/markdown-it/markdown-it/blob/master/docs/examples/renderer_rules.md#reusing-existing-rules
+const defaultParagraphOpenRenderer = markdownit.renderer.rules.paragraph_open || proxy;
+markdownit.renderer.rules.paragraph_open = function(tokens, idx, options, env, self) {
+   // Apply the Vuetify default body text class
+   // https://vuetifyjs.com/en/styles/text-and-typography/#typography
+   tokens[idx].attrJoin("class", 'text-body-1')
+   return defaultParagraphOpenRenderer(tokens, idx, options, env, self)
+};
+
+
+// Apply Vuetify text classes to heading elements
+// https://github.com/markdown-it/markdown-it/blob/master/docs/examples/renderer_rules.md#reusing-existing-rules
+const defaultHeadingOpenRenderer = markdownit.renderer.rules.heading_open || proxy;
+
+markdownit.renderer.rules.heading_open = function(tokens, idx, options, env, self) {
+   // Apply the appropriate Vuetify typography class, for ex. `text-h2`
+   // https://vuetifyjs.com/en/styles/text-and-typography/#typography
+   tokens[idx].attrJoin("class", `text-${tokens[idx].tag}`)
+   // Add whitespace
+   tokens[idx].attrJoin("class", 'my-5')
+   return defaultHeadingOpenRenderer(tokens, idx, options, env, self)
+};
+
+// Wrap any HTML tables with Vuetify table classes
+// https://github.com/markdown-it/markdown-it/blob/master/docs/examples/renderer_rules.md#to-add-a-wrapper-element
+
+const defaultTableOpenRenderer = markdownit.renderer.rules.table_open || proxy;
+markdownit.renderer.rules.table_open = (tokens, idx, options, env, self) => {
+  return `<div class="v-table v-table--density-default"><div class="v-table__wrapper">${defaultTableOpenRenderer(tokens, idx, options, env, self)}`;
+};
+
+const defaultTableCloseRenderer = markdownit.renderer.rules.table_close || proxy;
+markdownit.renderer.rules.table_close = (tokens, idx, options, env, self) => {
+  return `${defaultTableCloseRenderer(tokens, idx, options, env, self)}</div></div>`;
+};
 
 export default {
   install: (app: App) => {
