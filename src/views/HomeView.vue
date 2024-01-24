90d107ea1a7fa279fd1ce9bb03e21a7d88453ca9
Lily Wong <lwong@mitre.org>
2024-01-24 15:50:21 -0500

    ATLAS Matrix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,16 @@
 <template>
   <div>
     <HomeHero/>
-    
-    <div
-      class="pt-10 pr-12 pl-12 pb-10"
-    >
-      <v-row
-        color="white"
-        tile
-        class="pr-12 pl-12 mb-5"
-        width="100%"
-      >
+
+    <v-container>
+      <v-row>
         <v-col :cols="mobile ? 12 : 9">
 
-          <div
-            class="text-black pt-8 p-10 mt-8 text-subtitle-1" 
-          >
+          <div class="my-10 text-subtitle-1">
             MITRE ATLAS™ (Adversarial Threat Landscape for Artificial-Intelligence Systems) is a globally accessible, living knowledge base of adversary tactics and techniques against Al-enabled svstems based on real-world attack observations and realistic demonstrations from Al red teams and security groups. ATLAS is modeled after and complementary to MITRE ATT&CK®, raising awareness of the rapidly evolving vulnerabilities of Al-enabled systems as they extend beyond cyber.
-        </div>
+          </div>
         </v-col>
-        <v-col class="pt-10 pr-12 pl-12 pb-10" :cols="`${mobile ? 12 : 3}`">
+        <v-col class="" :cols="`${mobile ? 12 : 3}`">
           <img
             justify="center"
             src="../assets/compass.png"
@@ -28,12 +19,14 @@
           />
         </v-col>
       </v-row>
-    </div>
+
+    <AtlasMatrix></AtlasMatrix>
+    </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import AtlasMatrix from '@/components/matrix/AtlasMatrix.vue';
 import HomeHero from '../components/HomeHero.vue';
 
 import { useDisplay } from 'vuetify'
