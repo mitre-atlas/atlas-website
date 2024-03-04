43556559f388991dfedcbc8c43c5539a05f5f2dc
Lily Wong <lwong@mitre.org>
2024-03-04 16:54:23 -0500

    Quick links section in about section, minor mobile adjustments

--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -4,19 +4,22 @@
       <v-col>
         <v-card flat>
           <v-card-title>
-            <v-img src="@/assets/graphics/MITRE_ATLAS_logo_stacked.svg" max-height="150" :style="centerImageOnMoblile"></v-img>
+            <v-img
+              src="@/assets/graphics/MITRE_ATLAS_logo_stacked.svg"
+              :max-height="imageMaxHeight"
+              :style="centerImageOnMoblile"
+            ></v-img>
           </v-card-title>
           <v-card-text>
-            <div class="text-body-1 my-5">
+            <div class="text-body-1 mt-5">
               ATLAS (Adversarial Threat Landscape for Artificial-Intelligence Systems) is a globally
               accessible, living knowledge base of adversary tactics and techniques against
               Al-enabled svstems based on real-world attack observations and realistic
               demonstrations from Al red teams and security groups.
             </div>
           </v-card-text>
-          <v-card-actions>
+          <v-card-actions style="justify-content: center">
             <v-btn
-              :class="centerButtonOnMobile"
               variant="outlined"
               href="https://atlas.mitre.org/pdf-files/MITRE_ATLAS_Fact_Sheet.pdf"
               append-icon="mdi-chevron-right"
@@ -27,23 +30,23 @@
       </v-col>
 
       <v-col>
-        <v-row>
+        <v-row :class="verticalSepOnMobile">
           <v-col>
-            <div class="text-h6">Quick Links</div>
+            <div class="text-h6 text-center">Quick Links</div>
           </v-col>
         </v-row>
-        <v-row :class="verticalSepOnMobile">
-          <!-- <iframe
-            class="mx-auto"
-            width="560"
-            height="315"
-            src="https://www.youtube-nocookie.com/embed/3FN9v-y-C-w"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe> -->
+        <v-row>
           <v-col v-for="link in quickLinks" :key="link.title" cols="6">
-            <v-btn :prepend-icon="link.icon" variant="text" class="text-capitalize" :size="linkSizeOnMobile" :to="link.link">{{ link.title }}</v-btn>
+            <v-btn
+              :prepend-icon="link.icon"
+              variant="text"
+              :class="`text-none ${smAndUp ? 'text-body-1' : 'text-body-2'}`"
+              style="justify-content: unset"
+              :size="linkSizeOnMobile"
+              :to="link.link"
+              block
+              >{{ link.title }}</v-btn
+            >
           </v-col>
         </v-row>
       </v-col>
@@ -55,11 +58,11 @@
 import { computed } from 'vue'
 import { useDisplay } from 'vuetify'
 
-const { mdAndUp } = useDisplay()
+const { smAndUp, mdAndUp } = useDisplay()
 
 // About section
-const centerButtonOnMobile = computed(() => (mdAndUp.value ? '' : ''))
-const verticalSepOnMobile = computed(() => (mdAndUp.value ? '' : 'mt-10'))
+const imageMaxHeight = computed(() => (mdAndUp.value ? 150 : 100))
+const verticalSepOnMobile = computed(() => (mdAndUp.value ? '' : 'mt-5'))
 const centerImageOnMoblile = computed(() => (mdAndUp.value ? '' : '{ width: unset !important; }'))
 // Quick links
 const linkSizeOnMobile = computed(() => (mdAndUp.value ? 'large' : 'small'))
@@ -67,57 +70,33 @@ const linkSizeOnMobile = computed(() => (mdAndUp.value ? 'large' : 'small'))
 const quickLinks = [
   {
     icon: 'mdi-account-circle',
-    color: 'blue',
-    title: 'Contact us',
-    link: '/resources/contact',
+    title: 'Contact Us',
+    link: '/resources/contact'
   },
   {
     icon: 'mdi-school',
-    color: 'blue',
     title: 'AI Security 101',
-    link: '/resources/ai-security-101',
+    link: '/resources/ai-security-101'
   },
   {
     icon: 'mdi-calendar',
-    color: 'blue',
     title: 'Upcoming events',
-    link: '/resources/events',
-  },
-  {
-    icon: 'mdi-view-grid-plus',
-    color: 'blue',
-    title: 'See what\'s new',
-    link: '/resources/updates',
+    link: '/resources/events'
   },
   {
     icon: 'mdi-file-download',
-    color: 'blue',
     title: 'Get ATLAS Data',
-    link: '/resources/info',
-  },
-  {
-    icon: 'mdi-briefcase',
-    color: 'blue',
-    title: 'View case studies',
-    link: '/studies',
-  },
-  {
-    icon: 'mdi-flag',
-    color: 'blue',
-    title: 'View all tactics',
-    link: '/tactics',
-  },
-  {
-    icon: 'mdi-strategy',
-    color: 'blue',
-    title: 'View all techniques',
-    link: '/techniques',
+    link: '/resources/info'
   },
   {
     icon: 'mdi-help',
-    color: 'blue',
     title: 'Terminology and FAQ',
-    link: '/resources/faq',
+    link: '/resources/faq'
+  },
+  {
+    icon: 'mdi-strategy',
+    title: 'View all techniques',
+    link: '/techniques'
   }
 ]
 </script>
