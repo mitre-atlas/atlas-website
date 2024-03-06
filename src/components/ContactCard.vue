99c05559053b21801b2342e52c297bb24f5fd0b1
Lily Wong <lwong@mitre.org>
2024-03-06 16:11:24 -0500

    Mobile-friendly contacts

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,44 +1,33 @@
 <template>
-    <v-card
-    class="pa-4"
-    :color="item.color"
+  <v-card
+    class="pa-4 d-flex flex-column fill-height rounded-lg"
     :href="item.link"
     target="_blank"
-    >
+    dark
+    variant="flat"
+    color="grey-lighten-4"
+  >
     <div class="d-flex flex-no-wrap align-center">
-      <div
-        size="72"
-        >
-        <v-icon
-          size="large"
-        >
+      <div size="72">
+        <v-icon size="large" :color="item.color">
           {{ item.icon }}
         </v-icon>
-    </div>
+      </div>
 
       <div>
-        <v-card-title
-          class="text-h6 pt-0 text-wrap"
-          v-text="item.title"
-          >
-        </v-card-title>
-        <v-card-subtitle
-          class="text-body-1 text-white text-wrap"
-        >
+        <v-card-title class="text-h6 pt-0 text-wrap" v-text="item.title"> </v-card-title>
+        <v-card-subtitle class="text-body-1 text-wrap">
           {{ item.text }}
         </v-card-subtitle>
       </div>
     </div>
   </v-card>
 </template>
-  
-<script setup>  
-  /**
-   * List of information to be rendered on the card
-   * @type {Object}
-   */
-  const { item } = defineProps(["item"]);
-  
+
+<script setup>
+/**
+ * List of information to be rendered on the card
+ * @type {Object}
+ */
+const { item } = defineProps(['item'])
 </script>
-  
-  
