1474793fea3f3262e6c5c268281e839861839cf9
henrychoy <hchoy@mitre.org>
2024-03-11 17:20:07 -0400

    add google analystics again

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { loadFonts } from './plugins/webfontloader'
 import markdownit from './plugins/markdownit'
 import { useMain } from "@/stores/main";
 import { createHead } from '@unhead/vue'
+import VueGtag from "vue-gtag"
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -17,6 +18,11 @@ app.use(vuetify);
 app.use(markdownit);
 app.use(pinia);
 app.use(head)
+if(import.meta.env.VITE_ANALYTICS_ID) {
+  app.use(VueGtag, {
+    config: { id: import.meta.env.VITE_ANALYTICS_ID }
+  })
+}
 
 
 // Initialize store
